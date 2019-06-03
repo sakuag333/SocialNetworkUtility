2e8670271273a7411a8cdb5426f6d8527cac362a
Sandesh Agrawal <sakuag333@gmail.com>
2019-06-03 16:03:08 +0530

    Identifying a post and reducing its opacity.
    Issues:
    > Does not work when clicked on heart/upvote icon
    > Javascript keeps injected to the redirected page that comes after clicking the post.

--- inject.js
+++ inject.js
@@ -3,7 +3,24 @@ if (!event) {return;}
 var posts = document.querySelectorAll('div[postid]');
 for (var i = 0; i<posts.length; i++) {
 if (posts[i].contains(event.target)) { 
-alert(posts[i].getAttribute('postid'));
+posts[i].className += ' xyz';
+//alert(posts[i].getAttribute('postid'));
 }
 }  
 });
+
+/*
+document.body.addEventListener('click', function(event) {
+if (!event) {return;}
+var post = document.body.querySelector("div[postid='231cd3d019']");
+try {
+if (post.contains(event.target)) { 
+console.log(post.innerHTML);
+}
+}
+catch (err) {
+alert(post);
+}
+});
+*/
+
